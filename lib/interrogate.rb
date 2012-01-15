3eceb496a640d52636af212ae04c80df90223788
Morgan Brown <brown.mhg@gmail.com>
2012-01-15 11:13:06 -0800

    Add module comment
    Edit some other comments

--- lib/interrogate.rb
+++ lib/interrogate.rb
@@ -1,10 +1,13 @@
 require "interrogate/version"
 
+# String?(\"Hello\")
+# Interrogate attempts to bring Scheme-like class predication to Ruby.
+# It provides an alternate syntax using Module#===
 module Interrogate
 
   # Given an interrogatory method name and arguments,
   # determine which class we are asking about and
-  # ask each arg if it is this class.
+  # ask each arg if it is an instance of this class.
   def self.interrogate(meth, *args)
     raise ArgumentError, "#{method}: wrong number of arguments (#{args.length} for 1)" if args.length < 1
     klass_name = meth.to_s.tr("?", "")
@@ -18,6 +21,8 @@ class Object
 
   # If the method name is interrogatory, then
   # try to interrogate the arguments in question.
+  # Otherwise, perform the default method missing
+  # behavior.
   def self.method_missing(method, *args, &block)
     if /^[A-Z].*\?$/ === method.to_s
       Interrogate.interrogate(method, *args)
@@ -28,6 +33,8 @@ class Object
 
   # If the method name is interrogatory, then
   # try to interrogate the arguments in question.
+  # Otherwise, perform the default method missing
+  # behavior.
   def method_missing(method, *args, &block)
     if /^[A-Z].*\?$/  === method.to_s
       Interrogate.interrogate(method, *args)
