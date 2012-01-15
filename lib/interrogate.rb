b0a88ac213a87b062488192e986e1b4c3d01ea66
Morgan Brown <brown.mhg@gmail.com>
2012-01-15 11:17:37 -0800

    Fix a problem with the method argument for interrogate

--- lib/interrogate.rb
+++ lib/interrogate.rb
@@ -8,9 +8,9 @@ module Interrogate
   # Given an interrogatory method name and arguments,
   # determine which class we are asking about and
   # ask each arg if it is an instance of this class.
-  def self.interrogate(meth, *args)
+  def self.interrogate(method, *args)
     raise ArgumentError, "#{method}: wrong number of arguments (#{args.length} for 1)" if args.length < 1
-    klass_name = meth.to_s.tr("?", "")
+    klass_name = method.to_s.tr("?", "")
     klass = Object.const_get(klass_name)
     args.inject(true){|bool, obj| bool && klass === obj}
   end
